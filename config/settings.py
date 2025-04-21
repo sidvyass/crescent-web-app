2caacc22fbd548d24c169b9d3aa38b9c6b372d09
sidvyass <sidd.vyass@gmail.com>
2025-04-21 00:59:30 -0400

    database config fix

--- config/settings.py
+++ config/settings.py
@@ -135,9 +135,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
